ee593749b409443765a968c6b2d7f85ea34782fe
Wei Sun <swaysty@gmail.com>
2020-03-26 20:41:28 +0800

    update deploy script

--- mydeploy.sh
+++ mydeploy.sh
@@ -9,7 +9,9 @@ git init
 git add -A
 git commit -m 'deploy'
 
-
+# Deploy to Github Pages
 git push -f git@github.com:sunwayihep/SunsKitchen.git master:gh-pages
 
+# Deploy to Gitee Pages
+git push -f git@gitee.com:sunwayihep/SunsKitchen.git master:gh_pages
 cd -
